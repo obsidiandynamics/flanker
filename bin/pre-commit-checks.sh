6a6172e948105337579902a64363a24a93f0bb7f
Emil Koutanov <ekoutanov@gmail.com>
2022-10-24 10:54:35 +1100

    Doc failure on warnings

--- bin/pre-commit-checks.sh
+++ bin/pre-commit-checks.sh
@@ -4,6 +4,5 @@ set -e
 cargo test
 cargo test --examples
 $(dirname "$0")/clippy-pedantic.sh
-cargo doc --no-deps
+RUSTDOCFLAGS="-D warnings" cargo doc --no-deps
 cargo bench --no-run --profile dev
-
